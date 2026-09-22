40cc7fc8b47923a31b35971bbbf745ae1a3ad399
satyaidk <nikadisatyanarayana@gmail.com>
2026-09-22 13:30:11 +0530

    anim_dull.h animation ( normal expression with dull face ) updated

--- animations.h
+++ animations.h
@@ -16,7 +16,7 @@
 #include "anim_music.h"
 #include "anim_sleepy.h"
 #include "anim_happy.h"
-#include "anim_angry.h"
+#include "anim_dull.h"
 #include "anim_angry2.h"
 #include "anim_speed.h"
 #include "anim_sleepy3.h"
@@ -41,7 +41,7 @@ const Anim ANIMS[] = {
   { "Music      ", music_data, music_offsets, MUSIC_FRAMES },
   { "Sleepy     ", sleepy_data, sleepy_offsets, SLEEPY_FRAMES },
   { "Happy      ", happy_data, happy_offsets, HAPPY_FRAMES },
-  { "Angry      ", angry_data, angry_offsets, ANGRY_FRAMES },
+  { "Dull       ", dull_data, dull_offsets, DULL_FRAMES },
   { "Angry2     ", angry2_data, angry2_offsets, ANGRY2_FRAMES },
   { "Speed      ", speed_data, speed_offsets, SPEED_FRAMES },
   { "Sleepy2    ", sleepy3_data, sleepy3_offsets, SLEEPY3_FRAMES },
@@ -62,7 +62,7 @@ const uint8_t ANIM_COUNT = sizeof(ANIMS) / sizeof(ANIMS[0]);
 #define ANIM_MUSIC        9
 #define ANIM_SLEEPY       10
 #define ANIM_HAPPY        11
-#define ANIM_ANGRY        12
+#define ANIM_DULL         12
 #define ANIM_ANGRY2       13
 #define ANIM_SPEED        14
 #define ANIM_SLEEPY3      15
